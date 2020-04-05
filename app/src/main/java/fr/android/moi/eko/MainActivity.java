fcbd82056e264f60702e922ec4884b36dcf71eb1
ThomasVrd <36441593+ThomasVrd@users.noreply.github.com>
2020-04-05 19:27:40 +0200

    Merge branch 'master' of C:\Users\thoma\Desktop\wetransfer-deab99\Eko2 with conflicts.

--- app/src/main/java/fr/android/moi/eko/MainActivity.java
+++ app/src/main/java/fr/android/moi/eko/MainActivity.java
@@ -4,20 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 
-
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton homeButton;
+    ImageButton recettesButton;
+    ImageButton donnerButton;
+    ImageButton frigoButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /*homeButton = (ImageButton) findViewById(R.id.imageButton2);
-        recettesButton = (ImageButton) findViewById(R.id.imageButton5);
+        homeButton = (ImageButton) findViewById(R.id.ButtonHome);
+        recettesButton = (ImageButton) findViewById(R.id.ButtonRecettes);
         donnerButton = (ImageButton) findViewById(R.id.imageButton4);
         frigoButton = (ImageButton) findViewById(R.id.imageButton6);
 
@@ -25,56 +29,30 @@ public class MainActivity extends AppCompatActivity {
         homeButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View arg0) {
 
-                setContentView(R.layout.activity_donner2);
+                setContentView(R.layout.activity_donner);
             }
         });
 
         donnerButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View arg0) {
-                Intent intent = new Intent(MainActivity.this, DonnerActivity.class);
-                startActivity(intent);
+
+                setContentView(R.layout.activity_donner);
             }
         });
 
         recettesButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View arg0) {
 
-                setContentView(R.layout.activity_donner2);
+                setContentView(R.layout.activity_donner);
             }
         });
         frigoButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View arg0) {
 
-                setContentView(R.layout.activity_donner2);
+                setContentView(R.layout.activity_donner);
             }
-        });*/
+        });
 
-    }
-
-    public void myClickHandler(View view)
-    {
-        switch(view.getId())
-        {
-            case R.id.ButtonHome:
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
-                break;
-            case R.id.ButtonDonner:
-                Log.i("clique", "ok");
-                Intent intent1 = new Intent(getApplicationContext(), DonnerActivity.class);
-                Log.i("lance activity 1", "ok");
-                startActivity(intent1);
-                Log.i("lance activity 2", "ok");
-                break;
-            case R.id.ButtonRecettes:
-                //Intent intent2 = new Intent(MainActivity.this, DonnerActivity.class);
-                //startActivity(intent2);
-                break;
-            case R.id.ButtonFrigo:
-                //Intent intent3 = new Intent(MainActivity.this, DonnerActivity.class);
-                //startActivity(intent3);
-                break;
-        }
     }
 
 }
