505232a3348b021766db6f55ecf1da5fa9c01b3b
ThomasVrd <36441593+ThomasVrd@users.noreply.github.com>
2020-04-06 21:21:01 +0200

    added file Class

---
+++ app/src/main/java/fr/android/moi/eko/lecture.java
@@ -0,0 +1,29 @@
+package fr.android.moi.eko;
+import java.io.*;
+import java.util.*;
+
+public class lecture {
+    private Scanner x;
+
+    public void openFile(){
+        try{
+            x = new Scanner(new File("data.txt"))
+        }
+        catch(Exception e)
+            System.out.println("fichier introuvable");
+    }
+
+    public void readFile(){
+        while(x.hasNext()){
+            String a = x.next();
+            String b = x.next();
+            String c = x.next();
+
+            System.out.printf("%s %s %s\n", a, b, c);
+        }
+    }
+
+    public void closefile(){
+        x.close();
+    }
+}
