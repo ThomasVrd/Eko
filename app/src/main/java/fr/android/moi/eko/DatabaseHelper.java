355b3f7597f08bf323bb34c8cd289ff9cfd0bc4e
MariePlo <marieploteau22@gmail.com>
2020-04-06 15:49:24 +0200

    db dead

--- app/src/main/java/fr/android/moi/eko/DatabaseHelper.java
+++ app/src/main/java/fr/android/moi/eko/DatabaseHelper.java
@@ -12,7 +12,7 @@ import java.sql.Date;
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     public static final String db_name = "Eko.db";
-    public static final String table_name = "Frigo";
+    public static final String table_name = "FrigoBis";
     public static final String Produit_ID = "ID";
     public static final String Produit_nom = "Nom";
     public static final String Produit_marque = "Marque";
