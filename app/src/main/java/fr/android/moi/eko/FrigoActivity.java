080cc63b114f812a0064ba4f0ae36473abd1c5c5
ThomasVrd <36441593+ThomasVrd@users.noreply.github.com>
2020-04-08 23:51:10 +0200

    ere

--- app/src/main/java/fr/android/moi/eko/FrigoActivity.java
+++ app/src/main/java/fr/android/moi/eko/FrigoActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.io.File;
@@ -59,8 +60,12 @@ public class FrigoActivity extends AppCompatActivity {
         TextView produit1 = (TextView) findViewById(R.id.textView2);
         TextView produit2 = (TextView) findViewById(R.id.textView3);
         TextView produit3 = (TextView) findViewById(R.id.textView4);
+        ImageView imgp1 = (ImageView) findViewById(R.id.imageView2);
+        ImageView imgp2 = (ImageView) findViewById(R.id.imageView3);
+        ImageView imgp3 = (ImageView) findViewById(R.id.imageView5);
 
         String[] text = new String[]{"", "", ""};
+        String[] img = new String[]{"", "", ""};
         int i=0;
         try{
             x = new Scanner(this.getAssets().open("data.txt"));
@@ -68,7 +73,9 @@ public class FrigoActivity extends AppCompatActivity {
                 String a = x.next();
                 String b = x.next();
                 String c = x.next();
+
                 text[i] = text[i] + a + " " + b + " " + c + "\n  " ;
+                //img[i] = d;
                 i++;
 
             }
@@ -90,6 +97,11 @@ public class FrigoActivity extends AppCompatActivity {
         produit1.setText(text[0]);
         produit2.setText(text[1]);
         produit3.setText(text[2]);
+
+        imgp1.setImageResource(R.drawable.tomate);
+        imgp2.setImageResource(R.drawable.carotte);
+        imgp3.setImageResource(R.drawable.poulet);
+
 
     }
 }
